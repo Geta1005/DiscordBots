001ae3f73c3ba2ea7009a009fd6a26d19979427b
Geta <getatizcof@gmail.com>
2023-07-13 16:49:35 +0700

    Create main.py

---
+++ Discord.py/Bot_1/main.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ui import Button, View, Select
+
+token = 'YOURTOKENHERE'
+prefix = 'YOURPREFIXHERE'
+status = [
+  "online",
+  "game"
+]
+
+intents = discord.Intents.all();
+bot = commands.Bot(command_prefix=prefix, intents=intents)
+
+
+
+@bot.event
+async def on_ready():
+  await client.change_presence(status=discord.Status[status[0]],activity=discord.Game(status[1]))
+  print(str(gettimedate())+' | '+'Logged as {0.user}'.format(client))
+@bot.event
+async def on_message(message):
+    content = message.content
+    author = message.author
+  
+    if author == client.user:
+      return
+
+    def cmd(syntax):
+      if content.startswith(prefix+syntax):
+        return True
+
+    if cmd('ping'):
+      await message.reply('**Pong!** '+str(round(bot.latency*1000))+'ms')
+     
+bot.run(token)
