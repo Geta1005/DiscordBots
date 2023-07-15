bd212217f08b18a7bb4fe764b6463a5da4086f0a
Geta <getatizcof@gmail.com>
2023-07-15 23:44:55 +0700

    Update main.py

--- Discord.py/Bot_1/main.py
+++ Discord.py/Bot_1/main.py
@@ -12,12 +12,10 @@ status = [
 intents = discord.Intents.all();
 bot = commands.Bot(command_prefix=prefix, intents=intents)
 
-
-
 @bot.event
 async def on_ready():
-  await client.change_presence(status=discord.Status[status[0]],activity=discord.Game(status[1]))
-  print(str(gettimedate())+' | '+'Logged as {0.user}'.format(client))
+  await bot.change_presence(status=discord.Status[status[0]],activity=discord.Game(status[1]))
+  print(str(gettimedate())+' | '+'Logged as {0.user}'.format(bot))
 @bot.event
 async def on_message(message):
     content = message.content
